7da13f0c58d0a3ece9e77b8f52532be8e75d1c6b
bl-rd <william.c.reid@hotmail.co.uk>
2017-02-23 21:05:38 +0000

    Add extra haiku and get working with a random function

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 import angular from 'angular';
 import haikuSrc from './haiku';
+import { getRandomIntInclusive } from './helpers';
 
 (function() {
   angular.module('haiku', [])
     .component('haiku', {
       templateUrl: './templates/haiku.html',
       controller: function HaikuController() {
-        // wierd transpiler stuff happening here...
-        this.haiku = haikuSrc.haikuSrc;
+        let localHaiku = this.haiku = haikuSrc.haikuSrc;
+        this.randomHaiku = getRandomHaiku();
+        function getRandomHaiku() {
+          let random = getRandomIntInclusive(0, localHaiku.length);
+          return localHaiku[random];
+        }
       }
     });
 })();
--- src/haiku.js
+++ src/haiku.js
@@ -6,6 +6,30 @@ let haikuSrc = [
       'and realise my three children',
       'have been watching'
     ]
+  },
+  {
+    author: 'Masaoka Shiki',
+    haiku: [
+      'My life, -',
+      'How much more of it remains?',
+      'The night is brief'
+    ]
+  },
+  {
+    author: 'Kobayashi Issa',
+    haiku: [
+      'Don\'t weep, insects -',
+      'Lovers, stars themselves',
+      'must part'
+    ]
+  },
+  {
+    author: 'Murakami Kijo',
+    haiku: [
+      'First autumn morning',
+      'the mirror I stare into',
+      'shows my father\'s face.'
+    ]
   }
 ];
 
